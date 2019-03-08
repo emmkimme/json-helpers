73952f9c0c0c52345c5295e48ddff1f746edb532
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2019-03-08 13:50:05 +0100

    Add unit tests

--- test/json-parser.test.js
+++ test/json-parser.test.js
@@ -12,209 +12,175 @@ function ObjectEqual(a1, a2) {
 
 describe('JSON-Parser', () => {
 
-  describe('big json', () => {
-    it('JSON.stringify', () => {
-      console.time('JSON.stringify - big json');
-      let result = JSON.stringify(bigData);
-      console.timeEnd('JSON.stringify - big json');
+  // describe('big json', () => {
+  //   it('JSON.stringify', () => {
+  //     console.time('JSON.stringify - big json');
+  //     let result = JSON.stringify(bigData);
+  //     console.timeEnd('JSON.stringify - big json');
 
-      console.time('JSON.parse - big json');
-      let newbigdata = JSON.parse(result);
-      console.timeEnd('JSON.parse - big json');
-      assert(ObjectEqual(bigData, newbigdata));
-    });
+  //     console.time('JSON.parse - big json');
+  //     let newbigdata = JSON.parse(result);
+  //     console.timeEnd('JSON.parse - big json');
+  //     assert(ObjectEqual(bigData, newbigdata));
+  //   });
 
-    it('JSON-Parser.stringify', () => {
-      console.time('JSON-Parser.stringify - big json');
-      let result = json_tools.JSONParser.stringify(bigData);
-      console.timeEnd('JSON-Parser.stringify - big json');
+  //   it('JSON-Parser.stringify', () => {
+  //     console.time('JSON-Parser.stringify - big json');
+  //     let result = json_tools.JSONParser.stringify(bigData);
+  //     console.timeEnd('JSON-Parser.stringify - big json');
 
-      console.time('JSON-Parser.parse - big json');
-      let newbigdata = json_tools.JSONParser.parse(result);
-      console.timeEnd('JSON-Parser.parse - big json');
-      assert(ObjectEqual(bigData, newbigdata));
-    });
-  });
+  //     console.time('JSON-Parser.parse - big json');
+  //     let newbigdata = json_tools.JSONParser.parse(result);
+  //     console.timeEnd('JSON-Parser.parse - big json');
+  //     assert(ObjectEqual(bigData, newbigdata));
+  //   });
+  // });
 
-  describe('small json', () => {
-    let busEvent = {
-      channel: '/electron-common-ipc/myChannel/myRequest',
-      sender: {
-        id: 'MyPeer_1234567890',
-        name: 'MyPeer_customName',
-        process: {
-          type: 'renderer',
-          pid: 2000,
-          rid: 2,
-          wcid: 10,
-          testUndefined: undefined
-        },
-        testArrayUndefined: [12, "str", undefined, 3, null, "end"]
-      },
-      request: {
-        replyChannel: '/electron-common-ipc/myChannel/myRequest/replyChannel',
-      }
-    };
+  // describe('small json', () => {
+  //   let busEvent = {
+  //     channel: '/electron-common-ipc/myChannel/myRequest',
+  //     sender: {
+  //       id: 'MyPeer_1234567890',
+  //       name: 'MyPeer_customName',
+  //       process: {
+  //         type: 'renderer',
+  //         pid: 2000,
+  //         rid: 2,
+  //         wcid: 10,
+  //         testUndefined: undefined
+  //       },
+  //       testArrayUndefined: [12, "str", undefined, 3, null, "end"]
+  //     },
+  //     request: {
+  //       replyChannel: '/electron-common-ipc/myChannel/myRequest/replyChannel',
+  //     }
+  //   };
 
-    it('JSON.stringify - small json', () => {
+  //   it('JSON.stringify - small json', () => {
+  //     let result;
+  //     console.time('JSON.stringify - small json');
+  //     for (i = 0; i < 10000; ++i) {
+  //       result = JSON.stringify(busEvent);
+  //     }
+  //     console.timeEnd('JSON.stringify - small json');
+
+  //     let resultParse;
+  //     console.time('JSON.parse - small json');
+  //     for (i = 0; i < 10000; ++i) {
+  //       resultParse = JSON.parse(result);
+  //     }
+  //     console.timeEnd('JSON.parse - small json');
+  //   });
+
+  //   it('JSON-Parser.stringify - small json', () => {
+  //     let result;
+  //     console.time('JSON-Parser.stringify - small json');
+  //     for (i = 0; i < 10000; ++i) {
+  //       result = json_tools.JSONParser.stringify(busEvent);
+  //     }
+  //     console.timeEnd('JSON-Parser.stringify - small json');
+
+  //     let resultParse;
+  //     console.time('JSON-Parser.parse - small json');
+  //     for (i = 0; i < 10000; ++i) {
+  //       resultParse = json_tools.JSONParser.parse(result);
+  //     }
+  //     console.timeEnd('JSON-Parser.parse - small json');
+  //   });
+  // });
+
+  // describe('complex json', () => {
+  //   let busEvent = {
+  //     channel: '/electron-common-ipc/myChannel/myRequest',
+  //     sender: {
+  //       id: 'MyPeer_1234567890',
+  //       name: 'MyPeer_customName',
+  //       process: {
+  //         type: 'renderer',
+  //         pid: 2000,
+  //         rid: 2,
+  //         wcid: 10,
+  //         testUndefined: undefined
+  //       },
+  //       testArrayUndefined: [12, "str", undefined, 3, null, "end"],
+  //       testBuffer: Buffer.from('ceci est un test'),
+  //       testError: new Error('Test Error'),
+  //       testTypeError: new TypeError('Test TypeError'),
+  //     },
+  //     request: {
+  //       replyChannel: '/electron-common-ipc/myChannel/myRequest/replyChannel',
+  //       testDate: new Date()
+  //     }
+  //   };
+
+  //   it('JSON-Parser.stringify - complex json', () => {
+  //     let result;
+  //     console.time('JSON-Parser.stringify - complex json');
+  //     for (i = 0; i < 10000; ++i) {
+  //       result = json_tools.JSONParser.stringify(busEvent);
+  //     }
+  //     console.timeEnd('JSON-Parser.stringify - complex json');
+
+  //     let resultParse;
+  //     console.time('JSON-Parser.parse - complex json');
+  //     for (i = 0; i < 10000; ++i) {
+  //       resultParse = json_tools.JSONParser.parse(result);
+  //     }
+  //     console.timeEnd('JSON-Parser.parse - complex json');
+  //   });
+  // });
+
+  function TestTypeOf(myValue, nameTypeOf, compare) {
+    it(`JSON-Parser.stringify - ${nameTypeOf}`, () => {
       let result;
-      console.time('JSON.stringify - small json');
+      console.time(`JSON-Parser.stringify - ${nameTypeOf}`);
       for (i = 0; i < 10000; ++i) {
-        result = JSON.stringify(busEvent);
+        result = json_tools.JSONParser.stringify(myValue);
       }
-      console.timeEnd('JSON.stringify - small json');
+      console.timeEnd(`JSON-Parser.stringify - ${nameTypeOf}`);
+
+      console.time(`JSON.stringify - ${nameTypeOf}`);
+      for (i = 0; i < 10000; ++i) {
+        let localResult = JSON.stringify(myValue, (k,v) => v);
+        localResult;
+      }
+      console.timeEnd(`JSON.stringify - ${nameTypeOf}`);
 
       let resultParse;
-      console.time('JSON.parse - small json');
-      for (i = 0; i < 10000; ++i) {
-        resultParse = JSON.parse(result);
-      }
-      console.timeEnd('JSON.parse - small json');
-    });
-
-    it('JSON-Parser.stringify - small json', () => {
-      let result;
-      console.time('JSON-Parser.stringify - small json');
-      for (i = 0; i < 10000; ++i) {
-        result = json_tools.JSONParser.stringify(busEvent);
-      }
-      console.timeEnd('JSON-Parser.stringify - small json');
-
-      let resultParse;
-      console.time('JSON-Parser.parse - small json');
+      console.time(`JSON-Parser.parse - ${nameTypeOf}`);
       for (i = 0; i < 10000; ++i) {
         resultParse = json_tools.JSONParser.parse(result);
       }
-      console.timeEnd('JSON-Parser.parse - small json');
-    });
-  });
+      console.timeEnd(`JSON-Parser.parse - ${nameTypeOf}`);
 
-  describe('complex json', () => {
-    let busEvent = {
-      channel: '/electron-common-ipc/myChannel/myRequest',
-      sender: {
-        id: 'MyPeer_1234567890',
-        name: 'MyPeer_customName',
-        process: {
-          type: 'renderer',
-          pid: 2000,
-          rid: 2,
-          wcid: 10,
-          testUndefined: undefined
-        },
-        testArrayUndefined: [12, "str", undefined, 3, null, "end"],
-        testBuffer: Buffer.from('ceci est un test'),
-        testError: new Error('Test Error'),
-        testTypeError: new TypeError('Test TypeError'),
-      },
-      request: {
-        replyChannel: '/electron-common-ipc/myChannel/myRequest/replyChannel',
-        testDate: new Date()
-      }
-    };
-
-    it('JSON-Parser.stringify - complex json', () => {
-      let result;
-      console.time('JSON-Parser.stringify - complex json');
+      console.time(`JSON.parse - ${nameTypeOf}`);
       for (i = 0; i < 10000; ++i) {
-        result = json_tools.JSONParser.stringify(busEvent);
+        let localParse = JSON.parse(result, (k,v) => v);
+        localParse;
       }
-      console.timeEnd('JSON-Parser.stringify - complex json');
+      console.timeEnd(`JSON.parse - ${nameTypeOf}`);
 
-      let resultParse;
-      console.time('JSON-Parser.parse - complex json');
-      for (i = 0; i < 10000; ++i) {
-        resultParse = json_tools.JSONParser.parse(result);
-      }
-      console.timeEnd('JSON-Parser.parse - complex json');
+      assert(compare(myValue, resultParse));
     });
-  });
-
+  }
 
   describe('buffer json', () => {
     let myBuffer = Buffer.from('ceci est un test');
-
-    it('JSON-Parser.stringify - buffer json', () => {
-      let result;
-      console.time('JSON-Parser.stringify - buffer json');
-      for (i = 0; i < 10000; ++i) {
-        result = json_tools.JSONParser.stringify(myBuffer);
-      }
-      console.timeEnd('JSON-Parser.stringify - buffer json');
-
-      let resultParse;
-      console.time('JSON-Parser.parse - buffer json');
-      for (i = 0; i < 10000; ++i) {
-        resultParse = json_tools.JSONParser.parse(result);
-      }
-      console.timeEnd('JSON-Parser.parse - buffer json');
-
-      assert(myBuffer.compare(resultParse) === 0);
-    });
+    TestTypeOf(myBuffer, "Buffer", (r1, r2) => r1.compare(r2) === 0 );
   });
 
   describe('Date json', () => {
     let myDate = new Date();
-
-    it('JSON-Parser.stringify - date json', () => {
-      let result;
-      console.time('JSON-Parser.stringify - Date json');
-      for (i = 0; i < 10000; ++i) {
-        result = json_tools.JSONParser.stringify(myDate);
-      }
-      console.timeEnd('JSON-Parser.stringify - Date json');
-
-      let resultParse;
-      console.time('JSON-Parser.parse - Date json');
-      for (i = 0; i < 10000; ++i) {
-        resultParse = json_tools.JSONParser.parse(result);
-      }
-      console.timeEnd('JSON-Parser.parse - Date json');
-
-      assert(myDate.valueOf() == resultParse.valueOf());
-    });
+    TestTypeOf(myDate, "Date", (r1, r2) => r1.valueOf() == r2.valueOf() );
   });
 
   describe('Error json', () => {
     let myError = new Error();
-
-    it('JSON-Parser.stringify - Error json', () => {
-      let result;
-      console.time('JSON-Parser.stringify - Error json');
-      for (i = 0; i < 10000; ++i) {
-        result = json_tools.JSONParser.stringify(myError);
-      }
-      console.timeEnd('JSON-Parser.stringify - Error json');
-
-      let resultParse;
-      console.time('JSON-Parser.parse - Error json');
-      for (i = 0; i < 10000; ++i) {
-        resultParse = json_tools.JSONParser.parse(result);
-      }
-      console.timeEnd('JSON-Parser.parse - Error json');
-
-      assert(myError.message == resultParse.message);
-    });
+    TestTypeOf(myError, "Error", (r1, r2) => r1.message == r2.message );
   });
 
   describe('TypeError json', () => {
-    let myTypeError = new TypeError();
-
-    it('JSON-Parser.stringify - TypeError json', () => {
-      let result;
-      console.time('JSON-Parser.stringify - TypeError json');
-      for (i = 0; i < 10000; ++i) {
-        result = json_tools.JSONParser.stringify(myTypeError);
-      }
-      console.timeEnd('JSON-Parser.stringify - TypeError json');
-
-      let resultParse;
-      console.time('JSON-Parser.parse - TypeError json');
-      for (i = 0; i < 10000; ++i) {
-        resultParse = json_tools.JSONParser.parse(result);
-      }
-      console.timeEnd('JSON-Parser.parse - TypeError json');
-
-      assert(myTypeError.message == resultParse.message);
-    });
-  });});
+    let myError = new TypeError();
+    TestTypeOf(myError, "TypeError", (r1, r2) => r1.message == r2.message );
+  });
+});
