5ac42ed9a7456de5c8484d82b5eeba88c56a6223
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2019-06-26 16:51:58 +0200

    Common entry points declaration

---
+++ src/json-helpers-common.ts
@@ -0,0 +1,6 @@
+
+export * from './json-parser';
+import * as v1 from './tojson-v1';
+v1;
+import * as v2 from './tojson-v2';
+v2;
--- src/json-helpers.ts
+++ src/json-helpers.ts
@@ -1,6 +1,1 @@
-
-export * from './json-parser';
-import * as v1 from './tojson-v1';
-v1;
-import * as v2 from './tojson-v2';
-v2;
+export * from './json-helpers-common';
